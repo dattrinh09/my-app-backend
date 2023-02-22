c4c9f7409e9e871cfdc27c474230259bb86c118e
dattrinh09 <tiendattrinh09@gmail.com>
2023-02-22 22:37:43 +0700

    Add user function

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, HttpCode, Post, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../user/user.dto';
+import { SignInDto } from './auth.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) { }
+
+  @Post('/signup')
+  @HttpCode(HttpStatus.CREATED)
+  async signUp(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.signUp(createUserDto);
+  }
+
+  @Post('/signin')
+  @HttpCode(HttpStatus.OK)
+  async signIn(@Body() signInDto: SignInDto) {
+    return await this.authService.signIn(signInDto);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,49 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './user.dto';
+import { users } from '@prisma/client';
+
+@Injectable()
+export class UserService {
+    constructor(private prisma: PrismaService) { }
+
+    async findUserByUsername(username: string) {
+        return await this.prisma.users.findUnique({
+            where: { username: username },
+        });
+    }
+
+    async findUserByEmail(email: string) {
+        return await this.prisma.users.findUnique({
+            where: { email: email },
+        });
+    }
+
+    async createUser(createUserDto: CreateUserDto) {
+        const existUser: users[] = await this.prisma
+            .$queryRaw`SELECT * FROM users WHERE username = ${createUserDto.username} OR email= ${createUserDto.email}`;
+
+        if (existUser.length > 0) {
+            throw new HttpException(
+                { message: 'User already exist' },
+                HttpStatus.BAD_REQUEST,
+            );
+        }
+
+        return await this.prisma.users.create({ data: { ...createUserDto } });
+    }
+
+    async getAllUsers() {
+        return await this.prisma.users.findMany({ where: { is_admin: false } })
+    }
+
+    async deleteUserById(id: number) {
+        await this.prisma.users
+            .delete({ where: { id: id, } })
+            .catch((err) => {
+                throw new HttpException({ message: err }, HttpStatus.BAD_REQUEST);
+            });
+
+        return { message: 'Delete user success' };
+    }
+}
